ca3268ace92d82e3d6333254d23b6e627e639611
Baptiste Horn <baphorn@gmail.com>
2017-08-22 18:52:14 +0200

    I have to make the cards-container flex

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,5 +1,6 @@
 class ReportsController < ApplicationController
   before_action :set_report, only: [:show, :new, :update, :destroy]
+  skip_before_action :authenticate_user!, only: [:show, :index]
 
   def index
     @reports = Report.all

92080556763405364abc8497cee0607d30c7cb58
adrienomont <adriomont@gmail.com>
2017-08-25 14:54:22 +0200

    city infos show

--- app/models/report.rb
+++ app/models/report.rb
@@ -2,8 +2,8 @@ class Report < ApplicationRecord
   belongs_to :degradation
   belongs_to :furniture
   belongs_to :city
-  # belongs_to :priority
-  # belongs_to :status
+  belongs_to :priority
+  belongs_to :status
   has_many :messages
   validates :submit_date, presence: true, allow_blank: false
   validates :address, presence: true, allow_blank: false

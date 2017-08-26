94049791daa9b5ba1a3434c82ee5584be730613d
Olivier Bellier <olivier@observices.fr>
2017-08-26 18:29:01 +0200

    correction calcul nb incident par status

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -3,13 +3,14 @@ class CitiesController < ApplicationController
   before_action :set_city, only: [:show, :update, :infos]
   skip_before_action :authenticate_user!, only: [:show, :index, :infos]
 
-  # def index
-  #   @cities = City.all
-  # end
-
   def show
     if current_user && current_user.city == @city
       @reports = @city.reports.all.where.not(report_latitude: nil, report_longitude: nil)
+      @status = Status.all
+      @attente = 0
+      @cours = 0
+      @prog = 0
+      @resolu = 0
       @hash = Gmaps4rails.build_markers(@reports) do |report, marker|
         marker.lat report.report_latitude
         marker.lng report.report_longitude
@@ -17,17 +18,45 @@ class CitiesController < ApplicationController
         # marker.infowindow render_to_string(partial: "/flats/map_box", locals: { flat: flat })
         case report.status_id
         # id: 1, name: "En attente de traitement"
-        when 1 then marker.picture({ url: ActionController::Base.helpers.asset_path("red.png"), width: 46, height: 46 })
+        when @status.where(name: "En attente de traitement").ids[0]
+        then marker.picture({ url: ActionController::Base.helpers.asset_path("red.png"), width: 46, height: 46 })
           # id: 2, name: "Commande en cours"
-        when 2 then marker.picture({ url: ActionController::Base.helpers.asset_path("yellow.png"), width: 46, height: 46 })
+        when @status.where(name: "Commande en cours").ids[0]
+        then marker.picture({ url: ActionController::Base.helpers.asset_path("yellow.png"), width: 46, height: 46 })
           # id: 3, name: "Intervention programmée"
-        when 3 then marker.picture({ url: ActionController::Base.helpers.asset_path("yellow.png"), width: 46, height: 46 })
+        when @status.where(name: "Intervention programmée").ids[0]
+        then marker.picture({ url: ActionController::Base.helpers.asset_path("yellow.png"), width: 46, height: 46 })
           # id: 4, name: "Intervention en cours"
-        when 4 then marker.picture({ url: ActionController::Base.helpers.asset_path("orange.png"), width: 46, height: 46 })
+        when @status.where(name: "Intervention en cours").ids[0]
+        then marker.picture({ url: ActionController::Base.helpers.asset_path("orange.png"), width: 46, height: 46 })
           # id: 5, name: "Résolu"
-        when 5 then marker.picture({ url: ActionController::Base.helpers.asset_path("green.png"), width: 46, height: 46 })
+        when @status.where(name: "Résolu").ids[0]
+        then marker.picture({ url: ActionController::Base.helpers.asset_path("green.png"), width: 46, height: 46 })
           # id: 6, name: "Annulé"
-        when 6 then marker.picture({ url: ActionController::Base.helpers.asset_path("lightblue.png"), width: 46, height: 46 })
+        when @status.where(name: "Annulé").ids[0]
+        then marker.picture({ url: ActionController::Base.helpers.asset_path("lightblue.png"), width: 46, height: 46 })
+        end
+
+        puts "_______#{report.status_id}_________"
+        case report.status_id
+        # id: 1, name: "En attente de traitement"
+        when @status.where(name: "En attente de traitement").ids[0]
+        then @attente += 1
+          # id: 2, name: "Commande en cours"
+        when @status.where(name: "Commande en cours").ids[0]
+        then @cours += 1
+          # id: 3, name: "Intervention programmée"
+        when @status.where(name: "Intervention programmée").ids[0]
+        then @prog += 1
+          # id: 4, name: "Intervention en cours"
+        when @status.where(name: "Intervention en cours").ids[0]
+        then @cours += 1
+          # id: 5, name: "Résolu"
+        when @status.where(name: "Résolu").ids[0]
+        then @resolu += 1
+          # id: 6, name: "Annulé"
+        when @status.where(name: "Annulé").ids[0]
+        then @resolu += 1
         end
       end
     else
@@ -48,26 +77,26 @@ class CitiesController < ApplicationController
 
   def infos
     @reports = @city.reports.all.where.not(report_latitude: nil, report_longitude: nil)
-      @hash = Gmaps4rails.build_markers(@reports) do |report, marker|
-        marker.lat report.report_latitude
-        marker.lng report.report_longitude
-        marker.infowindow render_to_string(partial: "/cities/map_box", locals: { report: report })
-        # marker.infowindow render_to_string(partial: "/flats/map_box", locals: { flat: flat })
-        case report.status_id
-        # id: 1, name: "En attente de traitement"
-        when 1 then marker.picture({ url: ActionController::Base.helpers.asset_path("red.png"), width: 46, height: 46 })
-          # id: 2, name: "Commande en cours"
-        when 2 then marker.picture({ url: ActionController::Base.helpers.asset_path("yellow.png"), width: 46, height: 46 })
-          # id: 3, name: "Intervention programmée"
-        when 3 then marker.picture({ url: ActionController::Base.helpers.asset_path("yellow.png"), width: 46, height: 46 })
-          # id: 4, name: "Intervention en cours"
-        when 4 then marker.picture({ url: ActionController::Base.helpers.asset_path("orange.png"), width: 46, height: 46 })
-          # id: 5, name: "Résolu"
-        when 5 then marker.picture({ url: ActionController::Base.helpers.asset_path("green.png"), width: 46, height: 46 })
-          # id: 6, name: "Annulé"
-        when 6 then marker.picture({ url: ActionController::Base.helpers.asset_path("lightblue.png"), width: 46, height: 46 })
-        end
+    @hash = Gmaps4rails.build_markers(@reports) do |report, marker|
+      marker.lat report.report_latitude
+      marker.lng report.report_longitude
+      marker.infowindow render_to_string(partial: "/cities/map_box", locals: { report: report })
+      # marker.infowindow render_to_string(partial: "/flats/map_box", locals: { flat: flat })
+      case report.status_id
+      # id: 1, name: "En attente de traitement"
+      when 1 then marker.picture({ url: ActionController::Base.helpers.asset_path("red.png"), width: 46, height: 46 })
+        # id: 2, name: "Commande en cours"
+      when 2 then marker.picture({ url: ActionController::Base.helpers.asset_path("yellow.png"), width: 46, height: 46 })
+        # id: 3, name: "Intervention programmée"
+      when 3 then marker.picture({ url: ActionController::Base.helpers.asset_path("yellow.png"), width: 46, height: 46 })
+        # id: 4, name: "Intervention en cours"
+      when 4 then marker.picture({ url: ActionController::Base.helpers.asset_path("orange.png"), width: 46, height: 46 })
+        # id: 5, name: "Résolu"
+      when 5 then marker.picture({ url: ActionController::Base.helpers.asset_path("green.png"), width: 46, height: 46 })
+        # id: 6, name: "Annulé"
+      when 6 then marker.picture({ url: ActionController::Base.helpers.asset_path("lightblue.png"), width: 46, height: 46 })
       end
+    end
   end
 
   private

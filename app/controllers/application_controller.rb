7fd97a99a4c435bbadcd2c91205f22937b57513e
Olivier Bellier <olivier@observices.fr>
2017-08-28 16:30:34 +0200

    reecriture des routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
   def after_sign_in_path_for(resource)
-    request.env['omniauth.origin'] || stored_location_for(resource) || city_path(current_user.city_id)
+    request.env['omniauth.origin'] || stored_location_for(resource) || city_path
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
   has_many :messages
   belongs_to :city
+  has_many :reports, through: :city
   # validates :email, presence: true, allow_blank: false
   # validates :encrypted_password, presence: true, allow_blank: false
 end
